55fc3665baee25999ec331da0868a69740f5e68f
Bunlong Heng <bheng.code@gmail.com>
2025-03-02 11:39:12 -0500

    Displaying names;

--- components/Planet.tsx
+++ components/Planet.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { Text } from "@react-three/drei";
 import * as THREE from "three";
 
 interface PlanetProps {
@@ -20,9 +21,15 @@ export default function Planet({ name, radius, texture, position, onClick }: Pla
     });
 
     return (
-        <mesh ref={meshRef} position={position} onClick={onClick}>
-            <sphereGeometry args={[radius, 32, 32]} />
-            <meshStandardMaterial map={new THREE.TextureLoader().load(texture)} />
-        </mesh>
+        <group position={position}>
+            <mesh ref={meshRef} onClick={onClick}>
+                <sphereGeometry args={[radius, 32, 32]} />
+                <meshStandardMaterial map={new THREE.TextureLoader().load(texture)} />
+            </mesh>
+            {/* Add text label above the planet */}
+            <Text position={[0, radius + 1, 0]} fontSize={radius * 0.5} color="white">
+                {name}
+            </Text>
+        </group>
     );
 }
